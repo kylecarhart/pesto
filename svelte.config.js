c1c3d90b4bc515a21b1ca1a1717135ab7c0f823e
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2022-06-03 10:50:08 -0400

    Fixed PostCSS, upgraded typescript

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,6 @@ const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: [
     preprocess({
       postcss: true,
