49ac28013ddab327b538178d1ca198a6ec88a953
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2022-06-03 11:51:19 -0400

    Added github link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import Calculator from "../components/Calculator.svelte";
+  import Github from "../icons/github.svelte";
 </script>
 
-<div
-  class="bg-slate-50 sm:p-6 sm:flex sm:items-center sm:justify-center sm:h-screen"
->
+<div class="bg-slate-50 sm:flex sm:items-center sm:justify-center sm:h-screen">
   <Calculator />
 </div>
+<a
+  class="hidden sm:block sm:absolute sm:bottom-5 sm:right-5"
+  href="https://www.github.com/kylecarhart"
+>
+  <Github />
+</a>
