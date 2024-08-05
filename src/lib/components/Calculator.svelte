ca810934304bc1960301980e2f765fe55ea7cb4f
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2024-08-04 22:40:43 -0400

    cleanup

--- src/lib/components/Calculator.svelte
+++ src/lib/components/Calculator.svelte
@@ -65,86 +65,59 @@
 	}
 </script>
 
-<div
-	class="py-8 sm:py-12 px-8 sm:px-16 rounded-md bg-white sm:shadow-lg sm:max-w-3xl flex flex-col"
+<form method="POST" class="grid sm:grid-cols-2 gap-2 my-8" use:enhance>
+	<Input
+		label="Basil"
+		unit="g"
+		bind:value={$form.basil}
+		onInput={() => recalc((1 / R_BASIL) * $form.basil)}
+		error={$errors.basil}
+		constraints={$constraints.basil}
+	/>
+	<Input
+		label="Pine nuts"
+		unit="g"
+		bind:value={$form.nuts}
+		onInput={() => recalc((1 / R_NUTS) * $form.nuts)}
+		error={$errors.nuts}
+		constraints={$constraints.nuts}
+	/>
+	<Input
+		label="Parmesan"
+		unit="g"
+		bind:value={$form.parm}
+		onInput={() => recalc((1 / R_PARM) * $form.parm)}
+		error={$errors.parm}
+		constraints={$constraints.parm}
+	/>
+	<Input
+		label="Pecorino"
+		unit="g"
+		bind:value={$form.pec}
+		onInput={() => recalc((1 / R_PEC) * $form.pec)}
+		error={$errors.pec}
+		constraints={$constraints.pec}
+	/>
+	<Input
+		label="Garlic"
+		unit="cloves"
+		bind:value={$form.garlic}
+		onInput={() => recalc((1 / R_GARLIC) * $form.garlic)}
+		error={$errors.garlic}
+		constraints={$constraints.garlic}
+	/>
+	<!-- <Input
+		label="Olive Oil"
+		unit="ml"
+		bind:value={$form.oil}
+		onInput={() => recalc((1 / R_OIL) * $form.oil)}
+		error={$errors.oil}
+		constraints={$constraints.oil}
+	/> -->
+</form>
+<button
+	class="bg-slate-100 text-slate-600 text-sm py-2 px-4 mb-6 rounded active:bg-slate-200"
+	on:click={reset}
 >
-	<h1 class="text-2xl pb-2">Pesto Calculator</h1>
-	<p class="text-sm text-slate-600">
-		Type any values into the boxes below to get the correct ratio of ingredients
-		for delicious pesto.
-		<br />
-		<em>Tip: 70 grams of basil makes about 1 pound of pasta.</em>
-	</p>
-	<form method="POST" class="grid sm:grid-cols-2 gap-2 my-8" use:enhance>
-		<Input
-			label="Basil"
-			unit="g"
-			bind:value={$form.basil}
-			onInput={() => recalc((1 / R_BASIL) * $form.basil)}
-			error={$errors.basil}
-			constraints={$constraints.basil}
-		/>
-		<Input
-			label="Pine nuts"
-			unit="g"
-			bind:value={$form.nuts}
-			onInput={() => recalc((1 / R_NUTS) * $form.nuts)}
-			error={$errors.nuts}
-			constraints={$constraints.nuts}
-		/>
-		<Input
-			label="Parmesan"
-			unit="g"
-			bind:value={$form.parm}
-			onInput={() => recalc((1 / R_PARM) * $form.parm)}
-			error={$errors.parm}
-			constraints={$constraints.parm}
-		/>
-		<Input
-			label="Pecorino"
-			unit="g"
-			bind:value={$form.pec}
-			onInput={() => recalc((1 / R_PEC) * $form.pec)}
-			error={$errors.pec}
-			constraints={$constraints.pec}
-		/>
-		<Input
-			label="Garlic"
-			unit="cloves"
-			bind:value={$form.garlic}
-			onInput={() => recalc((1 / R_GARLIC) * $form.garlic)}
-			error={$errors.garlic}
-			constraints={$constraints.garlic}
-		/>
-		<!-- <Input
-			label="Olive Oil"
-			unit="ml"
-			bind:value={$form.oil}
-			onInput={() => recalc((1 / R_OIL) * $form.oil)}
-			error={$errors.oil}
-			constraints={$constraints.oil}
-		/> -->
-	</form>
-	<button
-		class="bg-slate-100 text-slate-600 text-sm py-2 px-4 mb-6 rounded active:bg-slate-200"
-		on:click={reset}
-	>
-		Reset
-	</button>
-	<h2 class="text-md mb-1 font-semibold">Instructions:</h2>
-	<ol class="list-decimal list-inside text-sm text-slate-800 space-y-1">
-		<li>
-			In a mortar and pestle, grind nuts and garlic into a paste while adding a
-			pinch or two of kosher salt.
-		</li>
-		<li>
-			Add basil leaves to the mortar and grind to a very smooth paste. Take your
-			time!
-		</li>
-		<li>
-			Add the paste to a large bowl and combine with grated parmesan and
-			pecorino cheese.
-		</li>
-		<li>Finally, add extra-virgin olive oil to your desired consistency.</li>
-	</ol>
-</div>
+	Reset
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,34 @@
 <div
 	class="sm:bg-slate-50 flex flex-col sm:items-center sm:justify-center sm:min-h-screen relative"
 >
-	<Calculator formData={data.form} />
+	<div
+		class="py-8 sm:py-12 px-8 sm:px-16 rounded-md bg-white sm:shadow-lg sm:max-w-3xl flex flex-col"
+	>
+		<h1 class="text-2xl pb-2">Pesto Calculator</h1>
+		<p class="text-sm text-slate-600">
+			Type any values into the boxes below to get the correct ratio of
+			ingredients for delicious pesto.
+			<br />
+			<em>Tip: 70 grams of basil makes about 1 pound of pasta.</em>
+		</p>
+		<Calculator formData={data.form} />
+		<h2 class="text-md mb-1 font-semibold">Instructions:</h2>
+		<ol class="list-decimal list-inside text-sm text-slate-800 space-y-1">
+			<li>
+				In a mortar and pestle, grind nuts and garlic into a paste while adding
+				a pinch or two of kosher salt.
+			</li>
+			<li>
+				Add basil leaves to the mortar and grind to a very smooth paste. Take
+				your time!
+			</li>
+			<li>
+				Add the paste to a large bowl and combine with grated parmesan and
+				pecorino cheese.
+			</li>
+			<li>Finally, add extra-virgin olive oil to your desired consistency.</li>
+		</ol>
+	</div>
 	<a
 		class="block m-5 self-center sm:absolute sm:bottom-5 sm:right-5"
 		href="https://www.github.com/kylecarhart/pesto"
